e1fa5cc2e275bc90b5d0abd979fc5062f8a669e3
hashi7412 <egghead.kenzo@gmail.com>
2023-08-17 21:10:40 -0500

    Add table

--- src/components/custom/table/style.ts
+++ src/components/custom/table/style.ts
@@ -1,16 +1,38 @@
+import { GV } from "@/utils/style.util";
 import styled from "styled-components";
 
+export const StyledTh = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+`;
+
+export const StyledTd = styled.div<{align?: string}>`
+  flex: 1;
+  display: flex;
+  ${({ align }) => align ? `align-items: ${align};` : ``}
+`;
+
+export const StyledTr = styled.div`
+  display: flex;
+  gap: 1rem;
+  padding: 1rem 1.5rem;
+  background: #191919;
+  height: 4.5rem;
+`;
+
+export const StyledTable = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const TableContainer = styled.div`
-`
+  width: 100%;
+  border-radius: ${GV("radius-md")};
+  overflow: hidden;
 
-export const StyledTable = styled.table`
-`
-
-export const StyledTr = styled.tr`
-`
-
-export const StyledTh = styled.th`
-`
-
-export const StyledTd = styled.td`
-`
+  ${StyledTr}:nth-child(even) {
+    background: #131313;
+  }
+`;
---
+++ src/pages/public/home/explore/index.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import { Content, ContentColor, ExploreContainer, TabButton } from './style';
+import { Flex, Heading, P, Span } from '@/components/basic';
+import { Button, Icon } from '@/components/custom';
+import { GV } from '@/utils/style.util';
+import Table, { TableFieldInterface } from '@/components/custom/table';
+import { metaverses } from './mockdata';
+import { tokillo } from '@/utils/util';
+
+const Explore = () => {
+
+	const fields: TableFieldInterface[] = [
+		{
+			key: "name",
+			label: "Name",
+			render: (i, v) => (
+				<Flex $style={{
+					vAlign: "center"
+				}}>
+					<Flex $style={{
+						gap: "0.75rem",
+						vAlign: "center"
+					}}>
+						<img src={`public/imgs/${i.avatar}`} width={40} height={40} />
+						<Flex $style={{
+							fDirection: "column"
+						}}>
+							<Flex>
+								<Span $style={{
+									weight: "bold"
+								}}>{v}</Span>
+							</Flex>
+							<Flex>
+								{i.networks.map((network: string, i: number) => (
+									<React.Fragment>
+										<Flex $style={{
+											gap: "0.25rem",
+											vAlign: "center"
+										}}>
+											<img src={`public/imgs/chains/ethereum.png`} width={12} height={12} />
+											<Span $style={{
+												size: "12px"
+											}}>{network}</Span>
+										</Flex>
+									</React.Fragment>
+								))}
+							</Flex>
+						</Flex>
+					</Flex>
+				</Flex>
+			),
+			sort: (i: any, v: any) => v,
+			description: "This field is ..."
+		},
+		{
+			key: "balance",
+			label: "Balance",
+			render: (i, v) => (<Span $style={{ weight: "bold" }}>${tokillo(v)}</Span>),
+			sort: (i: any, v: any) => v,
+			description: "This field is ..."
+		},
+		{
+			key: "uaw",
+			label: "UAW",
+			render: (i, v) => (<Span $style={{ weight: "bold" }}>${tokillo(v)}</Span>),
+			sort: (i: any, v: any) => v,
+			description: "This field is ..."
+		},
+		{
+			key: "prouaw",
+			label: "%UAW",
+			render: (i, v) => (<Span $style={{ weight: "bold", color: v > 0 ? v === 0 ? "white" : "success" : "danger" }}>{v}%</Span>),
+			sort: (i: any, v: any) => v,
+			description: "This field is ..."
+		},
+		{
+			key: "volume",
+			label: "Volume",
+			render: (i, v) => (<Span $style={{ weight: "bold" }}>${tokillo(v)}</Span>),
+			sort: (i: any, v: any) => v,
+			description: "This field is ..."
+		},
+		{
+			key: "provolume",
+			label: "%Volume",
+			render: (i, v) => (<Span $style={{ weight: "bold", color: v > 0 ? v === 0 ? "white" : "success" : "danger" }}>{v}%</Span>),
+			description: "This field is ..."
+		},
+		{
+			key: "uaw24h",
+			label: "24HUAW",
+			render: (i, v) => (v),
+			sort: (i: any, v: any) => v,
+			description: "This field is ..."
+		},
+	]
+
+	return (
+		<ExploreContainer>
+			<Flex $style={{
+				fDirection: 'column',
+				vAlign: 'center',
+				gap: '32px',
+			}}>
+				<Heading level={3}><Content>Explore The Marketplace</Content></Heading>
+				<Flex $style={{ maxW: '700px' }}>
+					<P $style={{ size: '20px', align: 'center' }}>Curabitur tempor quis eros tempus lacinia. Nam bibendum pellentesque quam a convallis. Sed ut vulputate nisi. Integer in felis </P>
+				</Flex>
+			</Flex>
+			<Flex $style={{
+				fDirection: 'column',
+				hAlign: 'flex-start',
+				gap: '8px'
+			}}>
+				<Flex $style={{
+					fDirection: 'row',
+					gap: '20px'
+				}}>
+					<TabButton isSelected={true}>Metaverses</TabButton>
+					<TabButton isSelected={false}>NFTs</TabButton>
+					<TabButton isSelected={false}>DApps</TabButton>
+					<TabButton isSelected={false}>Blockchains</TabButton>
+				</Flex>
+				<Flex $style={{
+					fDirection: 'column',
+					p: '24px 0 0',
+					gap: '4px'
+				}}>
+
+					<Table
+						data={metaverses}
+						fields={fields}
+					/>
+				</Flex>
+			</Flex>
+			<Button $style={{
+				bg: GV('gradient'),
+				radius: '4px',
+				p: '8px 64px'
+			}}>View All Marketplace</Button>
+		</ExploreContainer>
+	)
+}
+
+export default Explore;
