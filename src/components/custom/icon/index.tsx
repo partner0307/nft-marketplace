2a5f5915986e04bd93487e32f3f71749e7cf7d6b
hashi7412 <egghead.kenzo@gmail.com>
2023-08-25 09:16:46 -0500

    Update: icon, variables

--- src/components/custom/icon/index.tsx
+++ src/components/custom/icon/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { IconWrapper, IconWrapperType } from "./style";
 
 const icons = {
+    'TestPlus': (
+        <svg width="100%" height="100%" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M9 0.5H7V7.5H0V9.5H7V16.5H9V9.5H16V7.5H9V0.5Z" fill="currentColor" />
+        </svg>
+    ),
     'Plus': (
         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M9 0.5H7V7.5H0V9.5H7V16.5H9V9.5H16V7.5H9V0.5Z" fill="#007AFC" />
