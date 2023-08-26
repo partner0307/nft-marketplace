c23f576bce1e41a6b1b4618f153d649dc98e0d31
hashi7412 <egghead.kenzo@gmail.com>
2023-08-26 02:55:26 -0500

    Update menu

--- src/pages/public/home/index.tsx
+++ src/pages/public/home/index.tsx
@@ -7,19 +7,8 @@ import Collection from "./collection";
 import Blog from "./blog";
 
 const HomePage = () => {
-    // const [loaded, setLoaded] = React.useState(false);
 
     useEffect(() => {
-        // const onPageLoad = () => {
-        //     setLoaded(true);
-        // }
-
-        // if (document.readyState === 'complete') {
-        //     onPageLoad();
-        // } else {
-        //     window.addEventListener("load", onPageLoad);
-        //     return window.removeEventListener("load", onPageLoad);
-        // }
     }, [])
     return (
         <HomeContainer>
