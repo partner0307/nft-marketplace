c1f1172ea93a523359d4288d6bca319e26f786ff
hashi7412 <egghead.kenzo@gmail.com>
2023-08-28 13:29:29 -0500

    Update: slider animation

--- src/pages/public/marketplace/index.tsx
+++ src/pages/public/marketplace/index.tsx
@@ -24,10 +24,10 @@ const Marketplaces = () => {
             </StatusBar>
             <Flex $style={{ gap: '4rem' }}>
                 <Flex $style={{ gap: '1.25rem' }}>
-                    <Link to={routerer('marketplaces', 'marketplace_metaverses')}><CustomTab isActive={location.pathname.includes('marketplace-metaverses') || (location.pathname.endsWith('marketplaces'))}>Metaverses</CustomTab></Link>
-                    <Link to={routerer('marketplaces', 'marketplace_nfts')}><CustomTab isActive={location.pathname.includes('marketplace-nfts')}>NFTs</CustomTab></Link>
-                    <Link to={routerer('marketplaces', 'marketplace_dapps')}><CustomTab isActive={location.pathname.includes('marketplace-dapps')}>DApps</CustomTab></Link>
-                    <Link to={routerer('marketplaces', 'marketplace_blockchains')}><CustomTab isActive={location.pathname.includes('marketplace-blockchains')}>Blockchains</CustomTab></Link>
+                    <Link to={routerer('marketplaces', 'metaverse')}><CustomTab isActive={location.pathname.includes('marketplace-metaverses') || (location.pathname.endsWith('marketplaces'))}>Metaverses</CustomTab></Link>
+                    <Link to={routerer('marketplaces', '_NFT')}><CustomTab isActive={location.pathname.includes('marketplace-nfts')}>NFTs</CustomTab></Link>
+                    <Link to={routerer('marketplaces', '_DAPP')}><CustomTab isActive={location.pathname.includes('marketplace-dapps')}>DApps</CustomTab></Link>
+                    <Link to={routerer('marketplaces', '_MARKETPLACE')}><CustomTab isActive={location.pathname.includes('marketplace-blockchains')}>Blockchains</CustomTab></Link>
                 </Flex>
                 <Flex $style={{ gap: '1.25rem', w: '100%' }}>
                     <Flex $style={{ flex: '1' }}>
